836f6321afcaed07ce2fa34e035fe68b1ca36a5d
Matteo <mkinkela123@gmail.com>
2023-11-17 22:06:36 +0100

    Make install script for new ubuntu

---
+++ install_script.sh
@@ -0,0 +1,48 @@
+﻿#!/bin/sh
+
+sudo apt-get install build-essential procps curl file git
+
+# Install HomeBrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Set Path of HomeBrew
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Install node
+brew install node
+
+# Install Yarn
+brew install yarn
+
+# Install Zsh and Oh My ZSH
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Docker
+brew install docker
+
+# Install Visual Studio Code
+sudo apt install snapd
+sudo snap install code --classic
+sudo snap install code-insiders --classic
+
+# Install nvm
+git clone https://github.com/nvm-sh/nvm.git .nvm
+echo 'source ~/.nvm/nvm.sh' >> ~/.zshrc
+
+# Install Viber
+wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+sudo apt install ./viber.deb
+rm ./viber.deb
+
+# Install Tidal
+wget https://github.com/Mastermindzh/tidal-hifi/releases/download/4.1.0/tidal-hifi_4.1.0_amd64.deb
+sudo apt install ./tidal-hifi_4.1.0_amd64.deb
+rm ./tidal-hifi_4.1.0_amd64.deb 
+
